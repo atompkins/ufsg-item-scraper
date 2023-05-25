45d8625ac6446c9fffc3242bd69b0f8c87d3c673
atompkins <atompkins@users.noreply.github.com>
2023-05-25 20:04:15 +0100

    clean

--- scraper.js
+++ scraper.js
@@ -1,8 +1,6 @@
 const init = require('./src/init');
 const stdout = require('./src/stdout');
 
-console.log(process.version);
-
 init()
   .then(() => stdout('Finished...'))
   .catch((e) => stdout(e));

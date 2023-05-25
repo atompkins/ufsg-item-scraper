ffc58537f983137fd7c5eeae64a6cb466700963c
atompkins <atompkins@users.noreply.github.com>
2023-05-25 19:54:57 +0100

    rate limiter

--- scraper.js
+++ scraper.js
@@ -1,6 +1,8 @@
 const init = require('./src/init');
 const stdout = require('./src/stdout');
 
+console.log(process.version);
+
 init()
   .then(() => stdout('Finished...'))
   .catch((e) => stdout(e));
